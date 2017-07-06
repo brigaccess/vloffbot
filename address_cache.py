409b0fe9b5703471b65c15b0df3d547175b1663c
Igor Neizvestniy <brigaccess@gmail.com>
2017-07-06 18:14:33 +1000

    /start: introduced deep linking

--- address_cache.py
+++ address_cache.py
@@ -1,8 +1,12 @@
+import re
 import requests
 import traceback
 
 from database import uses_db
 from models import Address
+import geocoder
+
+title_re = re.compile('<title>(?:\s*)(.+?)(?:\s*)&mdash;', flags=re.S)
 
 
 def get_address_url(address):
@@ -17,6 +21,38 @@ def get_address_url(address):
     except:
         traceback.print_exc()
         return None
+
+
+@uses_db
+def add_address_from_url(address_url, session=None):
+    if not address_url.startswith('/off/'):
+        address_url = '/off/' + address_url
+    if not address_url.endswith('.html'):
+        address_url += '.html'
+
+    stored_address = session.query(Address).filter(Address.url == address_url).one_or_none()
+    if stored_address is not None:
+        return stored_address.id
+
+    try:
+        print(address_url)
+        r = requests.get('http://www.vl.ru' + address_url)
+        if r.status_code == 200:
+            addr = title_re.search(r.text)
+            if addr:
+                addresses = geocoder.city_filter(geocoder.request(q=addr.group(1)))
+                if len(addresses) > 1:
+                    return None
+
+                address = geocoder.extract_streetname(addresses[0])
+                a = Address(address=address, url=address_url)
+                session.add(a)
+                session.commit()
+                session.flush()
+                return a.id
+    except Exception as e:
+        traceback.print_exc()
+    return None
 
 
 @uses_db
--- bot_modules/start.py
+++ bot_modules/start.py
@@ -1,5 +1,11 @@
+import base64
+import traceback
+from binascii import Error
+
+import address_cache
 from database import uses_db
-from models import Chat
+from models import Address, Chat
+from utils import subscribe, SubscribeStatus
 
 
 def initialize(tg):
@@ -14,4 +20,32 @@ def start_command(tg, msg, session=None):
         chat_data.active = True
         session.commit()
 
+    arguments = msg['text'].split(' ')[1:]
+    if len(arguments) == 1:
+        start_with_address(tg, msg, arguments)
+    else:
+        tg.send_message(chat_id, _('help.line_1'))
+
+
+@uses_db
+def start_with_address(tg, msg, arguments, session=None):
+    chat_id = msg['chat']['id']
+    try:
+        b64address = base64.b64decode(arguments[0]).decode("utf-8")
+        cached_address = address_cache.add_address_from_url(b64address)
+        if cached_address is not None:
+            address = session.query(Address).get(cached_address)
+            result = subscribe(chat_id, cached_address)
+            if result[0] is SubscribeStatus.already_watching:
+                tg.send_message(chat_id, _('watch.already_watching'), hide_keyboard=True)
+            elif result[0] is SubscribeStatus.done:
+                tg.send_message(chat_id, _('start.done').format(addresses=address.address), hide_keyboard=True)
+            elif result[0] is SubscribeStatus.done_with_blackouts:
+                tg.send_message(chat_id, _('start.done_with_blackouts').format(addresses=address.address) + result[1],
+                                hide_keyboard=True)
+            return
+    except Error as e:
+        pass
+    except Exception as e:
+        traceback.print_exc()
     tg.send_message(chat_id, _('help.line_1'))
