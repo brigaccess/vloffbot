145182bf664dfbccbf4f04b85c76ac760d2c722b
Igor Neizvestniy <brigaccess@gmail.com>
2017-07-06 18:12:52 +1000

    /watch: extracted subscribition code to utils

--- bot_modules/watch.py
+++ bot_modules/watch.py
@@ -2,9 +2,7 @@ import json
 import re
 
 from address_cache import cached_address
-from database import uses_db
-from models import Subscribition, BlackoutAddress, Blackout, Address
-from utils import format_blackout
+from utils import subscribe, SubscribeStatus
 import geocoder
 
 hasnumbers = re.compile('[0-9]')
@@ -67,37 +65,20 @@ def destiny_handler(tg, msg):
         tg.reply(chat_id, msg_id, _('watch.wat'), hide_keyboard=False, dialogue=True)
 
 
-@uses_db
-def add_watch(tg, chat_id, msg_id, address, session=None):
+def add_watch(tg, chat_id, msg_id, address):
     cached_id = cached_address(address)
     if cached_id is None:
         tg.set_handler(chat_id, None)
         tg.reply(chat_id, msg_id, _('watch.blocked_or_broken'), hide_keyboard=True)
         return
 
-    if session.query(Subscribition).filter(Subscribition.chat == chat_id,
-                                           Subscribition.address_id == cached_id).count() > 0:
-        tg.set_handler(chat_id, None)
-        tg.reply(chat_id, msg_id, _('watch.already_watching'), hide_keyboard=True)
-        return
-
-    w = Subscribition(chat=chat_id, address_id=cached_address(address))
-    session.add(w)
-    session.commit()
-
-    current_blackouts = session.query(Address, Blackout) \
-        .join(BlackoutAddress, Address.url == BlackoutAddress.address_url) \
-        .join(Blackout, BlackoutAddress.blackout_id == Blackout.id) \
-        .filter(Address.id == cached_id, Blackout.done == False)
-
-    result_text = ''
-
-    for item in current_blackouts:
-        result_text += '\n'
-        result_text += format_blackout(_('blackout.format_addressless'), [(item[0].address, item[0].url)], item[1])
-
     tg.set_handler(chat_id, None)
-    if len(result_text) > 0:
-        tg.reply(chat_id, msg_id, _('watch.done_with_blackouts') + result_text, hide_keyboard=True)
-    else:
+    result = subscribe(chat_id, cached_id)
+
+    if result[0] is SubscribeStatus.already_watching:
+        tg.reply(chat_id, msg_id, _('watch.already_watching'), hide_keyboard=True)
+    elif result[0] is SubscribeStatus.done:
         tg.reply(chat_id, msg_id, _('watch.done'), hide_keyboard=True)
+    elif result[0] is SubscribeStatus.done_with_blackouts:
+        tg.reply(chat_id, msg_id, _('watch.done_with_blackouts') + result[1], hide_keyboard=True)
+
--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+from enum import Enum
+
+from database import uses_db
+from models import Address, Blackout, BlackoutAddress, Subscribition
+
+
 def get_genitive(s):
     if s == 'Горячая вода':
         return 'Горячей воды'
@@ -57,3 +63,35 @@ def format_addresses(addresses):
         result = '[%s](http://www.vl.ru%s?utm_source=vloffbot)' % addresses[0]
     return result
 
+
+class SubscribeStatus(Enum):
+    already_watching = 1
+    done = 2
+    done_with_blackouts = 3
+
+
+@uses_db
+def subscribe(chat_id, address_id, session=None):
+    if session.query(Subscribition).filter(Subscribition.chat == chat_id,
+                                           Subscribition.address_id == address_id).count() > 0:
+        return SubscribeStatus.already_watching, None
+
+    w = Subscribition(chat=chat_id, address_id=address_id)
+    session.add(w)
+    session.commit()
+
+    current_blackouts = session.query(Address, Blackout) \
+        .join(BlackoutAddress, Address.url == BlackoutAddress.address_url) \
+        .join(Blackout, BlackoutAddress.blackout_id == Blackout.id) \
+        .filter(Address.id == address_id, Blackout.done == False)
+
+    result_text = ''
+
+    for item in current_blackouts:
+        result_text += '\n'
+        result_text += format_blackout(_('blackout.format_addressless'), [(item[0].address, item[0].url)], item[1])
+
+    if len(result_text) > 0:
+        return SubscribeStatus.done_with_blackouts, result_text
+    else:
+        return SubscribeStatus.done, None
